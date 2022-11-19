add72227838e419c50b162536049e9d052f562a6
Pheonix(RC) <rohith31ch@gmail.com>
2022-11-19 22:06:53 +0000

    Modified Hello World

--- corejava/src/com/rohith/corejava/HelloWorld.java
+++ corejava/src/com/rohith/corejava/HelloWorld.java
@@ -3,6 +3,6 @@ package com.rohith.corejava;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        System.out.println("hello world rohit");
     }
 }
